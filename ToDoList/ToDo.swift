63eaf39fbd7221e07637858c267770719b6f82d3
haleyysanchez <haleymsanchez910@gmail.com>
2020-06-08 13:22:40 -0400

    commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Haley Sanchez on 6/8/20.
+//  Copyright © 2020 Haley Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
