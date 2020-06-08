fdb82a5299ecc3a0655b193913c7c52eda0405f7
haleyysanchez <haleymsanchez910@gmail.com>
2020-06-08 11:47:42 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Haley Sanchez on 6/8/20.
+//  Copyright © 2020 Haley Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
